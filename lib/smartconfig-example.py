ca81d7fa83045132b51d1a492332e4d3fc802b89
J. Kenney <kenney@usna.edu>
2022-10-24 13:53:01 -0400

    Not best practice, but use local jquery

--- lib/smartconfig-example.py
+++ lib/smartconfig-example.py
@@ -51,7 +51,8 @@ fileMAP      = {'smartweb-logon.html': loginDir + 'smartweb-logon.html',
                 'smartweb-sha256.js': loginDir + 'smartweb-sha256.js',
                 'smartweb-icon.png': loginDir + 'smartweb-icon.png',
                 'smartweb-logon.css': loginDir + 'smartweb-logon.css',
-                'smartweb-avatar.png': loginDir + 'smartweb-avatar.png'
+                'smartweb-avatar.png': loginDir + 'smartweb-avatar.png',
+                'smartweb-jquery.js': loginDir + 'smartweb-jquery.js'
                }
 
 # if python files are asked for, check the following map of authorized
