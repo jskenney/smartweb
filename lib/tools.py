0255da2f8b41ce87230fc31ba9cdd51f61a8d3ae
J. Kenney <kenney@moboard.com>
2024-02-18 17:59:55 -0500

    allow for options and debugging in loading and saving

--- lib/tools.py
+++ lib/tools.py
@@ -3,15 +3,21 @@
 from json import loads, dumps
 
 # Save a dictionary to file
-def saveDB(filename, data):
+def saveDB(filename, data, display=False, format=False):
+    if display:
+        print('# Saving', filename)
     with open(filename, 'w') as f:
-        f.write(dumps(data))
+        if format:
+            f.write(dumps(data, indent=2))
+        else:
+            f.write(dumps(data))
 
 # Load a dictionary from json
-def loadDB(filename, default):
+def loadDB(filename, default, display=False):
+    if display:
+        print('# Loading', filename)
     try:
         with open(filename) as f:
             return loads(f.read())
     except:
         return default
-
